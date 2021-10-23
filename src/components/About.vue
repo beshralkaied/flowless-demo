fb16a49ec1834e11d3297016f05ca4201939a46b
beshr <besrh.alkaied@gmail.com>
2021-10-23 18:13:49 +0300

    start

---
+++ src/components/About.vue
@@ -0,0 +1,23 @@
+<template>
+<h1 class="font-bold text-2xl ">
+    About {{ name }}
+</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+import  { useStore } from 'vuex'
+export default defineComponent({
+    setup() {
+        const store = useStore()
+        const name = computed(() => {
+            return store.state.user.name
+        })
+        return{
+            store,
+            name,
+
+        }
+    },
+})
+</script>
